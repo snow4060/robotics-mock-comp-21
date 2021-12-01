6f3109282af6de7ba3504272c07f58fb3edb2784
snow4060 <91996758+snow4060@users.noreply.github.com>
2021-11-30 17:28:06 -0800

    Update november23.c

--- november23.c
+++ november23.c
@@ -9,7 +9,8 @@
 #define rightDistance 2
 // turn_with_gyro params: first is left motor speed, second is right motor speed
 
-void followWall_black (int, int); //distance, port#
+void followWall_black_R (int); //distance
+void followWall_black_L (int); //distance
 void followWallRight (double, int); //time in seconds, distance
 void followWallLeft (double, int);//time in seconds, distance
 void servo (int, int, int); //port, position, time in ms
@@ -23,56 +24,88 @@ int main() {
     //leftMotor = 0.7* rightMotor
     //code to make gyro work
     declare_motors(0, 1); 
-    int gyroZ = gyro_z();
-    calibrate_gyro(gyroZ);
+    calibrate_gyro(100);
     enable_servo(claw);
-
-    /*servo(claw, 400, 200);
-    straight(-80, 1500);
-    motor(leftMotor, -25);
-    msleep(45); //purpenticular alignment
+    servo(0, 1600, 100);
     
-    servo(claw, 1600, 200);*/
-    
-    followWall_black(2900, 2);
+    followWall_black_R(2900); //starting position alignment for consistency 
     motor(leftMotor, -50);
-    motor(rightMotor, 75);
-    msleep(900);
+    motor(rightMotor, 85);
+    msleep(900); //left turn
+
     straight(90, 1700);
-    followLineRight(3);
-    straight(90, 400);
-    motor(leftMotor, 90);
-    motor(rightMotor, -75);
-    msleep(850);
-    straight(90, 550);
+    followLineRight(3.1); 
+    straight(90, 100); //on way to green crew mates
+
+    //motor(leftMotor, 60);
+    //motor(rightMotor, -60);
+    //msleep(875);
+    turn_with_gyro(700, -700, 67);
+    straight(90, 1700); //turns and pushes green crew mates out 
     
-    straight(-100, 2600);
-    turn_with_gyro(-900, 400, 100);
-    straight(90, 450);
-    motor(leftMotor, 70);
-    msleep(150);
+    motor(leftMotor, 35);
+    motor(rightMotor, 100); 
+    msleep(2550);
+    straight(90, 2275); //turns left, going under the bridge to the other side
     
-   followWallLeft(6, 2100);
+    motor(leftMotor, -40);
+    motor(rightMotor, 60); 
+    msleep(1000);
+    straight(100, 1050); //left turn, pushes the red crewmates
+    straight(90, 3200);
+    
+    
+    
+    turn_with_gyro(900, 0, 80); //right turn, begins heading to med bay
+    straight(90, 3400);
+    turn_with_gyro(900, 0, 90); // seocnd right turn
+    
+    followWall_black_L(2000);//follows the boarder wall to the perimeter of medbay
+    
+    straight(80, 2700);
+    ao();
+    msleep(3100); //goes into med bay, waits 3s
 
 
+    straight(-90, 2800);
+    turn_with_gyro(100, -900, 90);
+    
+    followWall_black_R(2700);
+    
+    //*/
 
 
-    /*// go into ramp entrance
-    followWall_black(2700, 2);
+    /*
+    followWall_black_R(2700); //go into ramp entrance
     ramp();//goes up ramp + trash chute*/
+
+    //optional: go down ramp, return to starting box
     return 0;
 
 }
 
-void followWall_black(int distance, int port) {
+void followWall_black_R(int distance) {
     while (analog(topHat) < 3700) {
-        if (analog(port) > distance -350) {
+        if (analog(rightDistance) > distance -350) {
             motor(leftMotor, 13);
             motor(rightMotor, 100);
         }
-        if (analog(port) < distance) {
+        if (analog(rightDistance) < distance) {
             motor(leftMotor, 100);
             motor(rightMotor, 13);
+        }
+    }
+}
+
+void followWall_black_L(int distance) {
+    while (analog(topHat) < 3700) {
+        if (analog(leftDistance) > distance -350) {
+            motor(leftMotor, 100);
+            motor(rightMotor, 13);
+        }
+        if (analog(leftDistance) < distance) {
+            motor(leftMotor, 13);
+            motor(rightMotor, 100);
         }
     }
 }
@@ -80,31 +113,30 @@ void followWall_black(int distance, int port) {
 void followWallRight(double time, int distance) {
     double startTime = seconds();
     while (seconds()-startTime < time) {
-        if (analog(2) > distance -350) {
-            motor(leftMotor, 13);
+        if (analog(rightDistance) > distance -350) {
+            motor(leftMotor, 50);
             motor(rightMotor, 100);
         }
-        if (analog(2) < distance) {
+        if (analog(rightDistance) < distance) {
             motor(leftMotor, 100);
-            motor(rightMotor, 13);
+            motor(rightMotor, 50);
         }
     }
 }
 
 void followWallLeft(double time, int distance) {
-	double startTime = seconds();
-	while (seconds()-startTime < time) {
-		if (analog(1) > distance -350) {
+    double startTime = seconds();
+    while (seconds()-startTime < time) {
+        if (analog(leftDistance) > distance -350) {
             motor(leftMotor, 100);
-            motor(rightMotor, 13);
+            motor(rightMotor, 50);
         }
-        if (analog(1) < distance) {
-            motor(leftMotor, 13);
+        if (analog(leftDistance) < distance) {
+            motor(leftMotor, 50);
             motor(rightMotor, 100);
         }
     }
 }
-
 
 void servo(int port, int pos, int time) {
     set_servo_position(port, pos);
@@ -148,17 +180,16 @@ void straight(int speed, int time) {
 }
 
 void ramp() {
-    motor(leftMotor, 60);
-    motor(rightMotor, 80);
-    msleep(1800); //alignment
+    straight(80, 1800); //alignment
 
     turn_with_gyro(100, 1000, 80);//left turn 
     motor(leftMotor, 70);
     motor(rightMotor, 40);
     msleep(200);
-    followWallRight(5, 2900);
-    straight(80, 400);
-    turn_with_gyro(100, 900, 85);
+    followWallRight(4, 2900);
+    printf("done1");
+    //straight(90, 150);
+    turn_with_gyro(100, 900, 75);
     // while the bot detects the rail, follow line
     while (analog(leftDistance) > 600){
 
@@ -167,13 +198,13 @@ void ramp() {
             motor(rightMotor, 25);
         }
         if (analog(0) > 3500){
-            motor(leftMotor, 	25);
+            motor(leftMotor,    25);
             motor(rightMotor, 90);
         }
     }
     printf("done \n");
     straight(90, 150);
-    turn_with_gyro(400, 1000, 80); //left turn on bridge
-    followLineLeft(1); //straight
-    followLineRight(4);
+    turn_with_gyro(400, 1000, 95);
+    straight(50, 1000);//left turn on bridge
+    followLineRight(3.7);
 }
